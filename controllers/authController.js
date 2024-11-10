0f0558d3eccdc06ac2eda91582efb5b9164e8038
oliver <mahdig502@gmail.com>
2024-11-10 17:53:35 +0330

    Chapter 11: JWT Auth (section 1)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+
 const usersDB = {
   users: require("../model/users.json"),
   setUsers: function (data) {
@@ -5,7 +7,10 @@ const usersDB = {
   },
 };
 const bcrypt = require("bcrypt");
-
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const fsPromises = require("fs").promises;
+const path = require("path");
 const handleLogin = async (req, res) => {
   const { user, pwd } = req.body;
   if (!user || !pwd)
@@ -20,7 +25,36 @@ const handleLogin = async (req, res) => {
   const match = await bcrypt.compare(pwd, foundUser.password);
   if (match) {
     // create JWTs
-    res.json({ success: `Logged in ${user}` });
+    const accessToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "30s",
+      }
+    );
+    const refreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    // Saving refreshToken with current user
+    const otherUsers = usersDB.users.filter(
+      (person) => person.username !== foundUser.username
+    );
+    const currentUser = { ...foundUser, refreshToken };
+    usersDB.setUsers([...otherUsers, currentUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(usersDB.users)
+    );
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.json({ accessToken });
   } else {
     res.status(401).json({ message: "Unauthorized" });
   }
